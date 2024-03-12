7ea373b1343842f94f16a7870efffb9611638380
JONATAN_MAZUCA_VALENZUELA <a.21308051280382@cbtis128.edu.mx>
2024-03-12 18:26:21 +0000

    actualizado

--- lib/pantalla1_0382.dart
+++ lib/pantalla1_0382.dart
@@ -7,13 +7,24 @@ class Pantalla1_0382 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Card Colores mazuca 0382 "),
-        backgroundColor: Colors.red[900], // Cambio de color a rojo vino
+        title: Text("Card p1 Colores Mazuca_0382 "),
+        backgroundColor: Colors.blueGrey[900], // Cambio de color a gris oscuro
       ),
-      body: Center(),
+      body: Center(
+        child: Card(
+          elevation: 40,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          color: Colors.tealAccent[700], // Cambio de color a verde turquesa
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              "Card Mazuca_0382",
+              style: TextStyle(fontSize: 30, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
-///
-//ma
--- lib/pantallainicial_0382.dart
+++ lib/pantallainicial_0382.dart
@@ -7,8 +7,8 @@ class Pantallainicial_0382 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Card Container mazuca_0382"),
-        backgroundColor: Colors.red[900], // Cambio de color a rojo vino
+        title: const Text("Card Container Mazuca_0382"),
+        backgroundColor: Colors.green[900], // Cambio de color a verde oscuro
       ),
       body: Center(
         child: Column(
@@ -19,12 +19,19 @@ class Pantallainicial_0382 extends StatelessWidget {
                 Navigator.pushNamed(context, "/Pantalla1_0382");
               },
               child: const Text("Mover a Pantalla1_0382"),
+              style: ElevatedButton.styleFrom(
+                primary:
+                    Colors.amberAccent[700], // Cambio de color a amarillo ámbar
+              ),
             ),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, "/Pantalla2_0382");
               },
               child: const Text("Mover a Pantalla2_0382"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.orange[900], // Cambio de color a naranja oscuro
+              ),
             )
           ],
         ),
